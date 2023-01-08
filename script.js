b035eba41577f4b59575abe8d5d2cc7b98f89fde
Adam Dabre <adamdabre23@gmail.com>
2023-01-08 16:59:53 +0800

    Finished the game fucntion The game is now able to be played in the console need to add a UI and UX to the site with the HTML File

--- script.js
+++ script.js
@@ -15,14 +15,31 @@ function getComputerChoice() {
     }
     return computersChoice;
 }
-let computerSelection = getComputerChoice();
-let playerSelection = window.prompt("Select either Rock, Paper, Scissors");
-
 function playRound(computerSelection, playerSelection) {
     if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
         return "It was a draw you both selected " + computerSelection;
-    } else if (players.toLowerCase() === "rock" && computerSelection.toLowerCase == "sissors") {
+    } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
         return "You Won! rock beats scissors";
+    } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
+        return "You Won! Scissors beats Paper";
+    }
+    else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        return "You Won! Paper beats rock";
+    }
+    else {
+        return "you loose";
     }
 }
-playRound(computerSelection, playerSelection);
+let computerSelection = getComputerChoice();
+let playerSelection = "";
+
+
+function game(computerSelection, playerSelection) {
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = window.prompt("Select either Rock, Paper, Scissors");
+        let computerSelection = getComputerChoice();
+        console.log(playRound(computerSelection, playerSelection));
+        console.log("\n")
+    }
+}
+game(computerSelection, playerSelection);
