202f0cb53faf96bf8debb5998270042d1ba4c51e
Adam Dabre <adamdabre23@gmail.com>
2023-01-31 17:52:38 +0800

    Finished the game, fixed the bug with the wrong game verdict

--- script.js
+++ script.js
@@ -17,32 +17,31 @@ function getComputerChoice() {
 }
 
 function playRound(computerSelection, playerSelection, scores) {
+    let outcomeMessage = document.querySelector("#outcome");
     let usersScore = document.querySelector("#userScore");
     usersScore.innerHTML = scores[0];
 
     let CompScore = document.querySelector("#CompScore");
     CompScore.innerHTML = scores[1];
+
     if (computerSelection.toLowerCase() === playerSelection.toLowerCase()) {
-        let congrats = document.querySelector("#congrats");
-        congrats.innerHTML = "Draw!";
+        outcomeMessage.innerHTML = "It's a draw!";
         return scores;
     } else if (playerSelection.toLowerCase() === "rock" && computerSelection.toLowerCase() === "scissors") {
-        scores[0]++;
-        console.log(scores[0], scores[1]);
+        scores[0] = (scores[0]) + 1;
+        outcomeMessage.innerHTML = "You win! Rock beats Scissors.";
         return scores;
     } else if (playerSelection.toLowerCase() === "scissors" && computerSelection.toLowerCase() === "paper") {
-        scores[0]++;
-        console.log(scores[0], scores[1]);
+        scores[0] = scores[0] + 1;
+        outcomeMessage.innerHTML = "You win! Scissors beats Paper.";
         return scores;
-    }
-    else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
-        scores[0]++;
-        console.log(scores[0], scores[1]);
+    } else if (playerSelection.toLowerCase() === "paper" && computerSelection.toLowerCase() === "rock") {
+        scores[0] = scores[0] + 1;
+        outcomeMessage.innerHTML = "You win! Paper beats Rock.";
         return scores;
-    }
-    else {
-        scores[1]++;
-        console.log(scores[0], scores[1]);
+    } else {
+        scores[1] = scores[1] + 1;
+        outcomeMessage.innerHTML = "You lose. " + computerSelection + " beats " + playerSelection + ".";
         return scores;
     }
 }
@@ -67,15 +66,55 @@ rock.addEventListener('click', () => {
         congrats.innerHTML = "You lose ";
         return;
     }
+    let outcomeMessage = document.querySelector("#outcome");
+    let usersScore = document.querySelector("#userScore");
+    usersScore.innerHTML = scores[0];
+
+    let CompScore = document.querySelector("#CompScore");
+    CompScore.innerHTML = scores[1];
 });
 let paper = document.querySelector('#paper');
 paper.addEventListener('click', () => {
     let computerSelection = getComputerChoice();
-    playRound(computerSelection, "paper", scores);
+    scores = playRound(computerSelection, "paper", scores);
+    if (scores[0] > 6 || scores[1] > 6) {
+        scores[0] = 0;
+        scores[1] = 0;
+    }
+    if (scores[0] == 6) {
+        let congrats = document.querySelector("#congrats");
+        congrats.innerHTML = "Congratulations, you WON!";
+        return;
+    } else if (scores[1] == 6) {
+        let congrats = document.querySelector("#congrats");
+        congrats.innerHTML = "You lose ";
+        return;
+    }
+    let outcomeMessage = document.querySelector("#outcome");
+    let usersScore = document.querySelector("#userScore");
+    usersScore.innerHTML = scores[0];
+
+    let CompScore = document.querySelector("#CompScore");
+    CompScore.innerHTML = scores[1];
 });
 
 let scissors = document.querySelector('#scissors');
 scissors.addEventListener('click', () => {
     let computerSelection = getComputerChoice();
-    playRound(computerSelection, "scissors", scores);
+    scores = playRound(computerSelection, "scissors", scores);
+    if (scores[0] > 6 || scores[1] > 6) {
+        scores[0] = 0;
+        scores[1] = 0;
+    }
+    if (scores[0] == 6) {
+        let congrats = document.querySelector("#congrats");
+        congrats.innerHTML = "Congratulations, you WON!";
+        return;
+    } else if (scores[1] == 6) {
+        let congrats = document.querySelector("#congrats");
+        congrats.innerHTML = "You lose , Thanks for playing ";
+        return;
+    }
+    let outcomeMessage = document.querySelector("#outcome");
+
 });
